68a5762ce1a41d33cd6162f30bc740e6d8c9ba24
stopek <stopek.pawel@gmail.com>
2024-09-23 20:41:20 +0200

    fix: clean app component, connect with state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenuComponent } from './core/components/menu/menu.component';
+import { GLOBAL_RX_STATE, GlobalState } from './store/rx-state';
+import { PeriodicService } from './core/api/periodic.service';
+import { RxState } from '@rx-angular/state';
+import { delay } from 'rxjs';
+
+// delay time before load data (in ms)
+const DELAY = 2000;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'app';
+  constructor(
+    @Inject(GLOBAL_RX_STATE) private state: RxState<GlobalState>,
+    private periodicService: PeriodicService
+  ) {
+    this.state.connect(
+      'periodic',
+      this.periodicService.fetchAll().pipe(delay(DELAY))
+    );
+  }
 }
