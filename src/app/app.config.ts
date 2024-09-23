5e6dbda718d3cdb7daeb59e188c787345367c779
stopek <stopek.pawel@gmail.com>
2024-09-23 20:37:46 +0200

    feat: create global state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { RxState } from '@rx-angular/state';
+import { GLOBAL_RX_STATE, GlobalState } from './store/rx-state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withDebugTracing()),
     provideAnimationsAsync(),
+    {
+      provide: GLOBAL_RX_STATE,
+      useFactory: () => new RxState<GlobalState>(),
+    },
   ],
 };
