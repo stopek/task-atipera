aa31c26f1a3a5b20904da2792966488066418f66
stopek <stopek.pawel@gmail.com>
2024-09-23 23:48:52 +0200

    fix: remove redundant provider (animations)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { RxState } from '@rx-angular/state';
 import { GLOBAL_RX_STATE, GlobalState } from './store/rx-state';
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withDebugTracing()),
-    provideAnimationsAsync(),
     {
       provide: GLOBAL_RX_STATE,
       useFactory: () => new RxState<GlobalState>(),
